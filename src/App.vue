9b428cc17d8c7ff31f0a1ff492ca85c693407880
rajabovdavlat <rajabovoff1@gmail.com>
2025-12-27 13:43:32 +0500

    Launching a Vue project — store

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+
+    <header class='flex justify-between border-b border-slate-300 px-8 py-8'>
+<div class='flex items-center gap-4'>
+    <img src="/logo.png" alt="Logo" class='w-10'>
+    <div>
+        <h2 class='text-xl font-bold uppercase'>Vue Sneakers</h2>
+        <p class='text-slate-400'>Магазин лучших крассовок</p>
+    </div>
+</div>
+
+<ul class='flex items-center gap-10'>
+    <li class='flex items-center gap-3'>
+        <img src="/cart.svg" alt="Cart">
+        <b>1285 руб.</b>
+    </li>
+
+     <li class='flex items-center gap-3'>
+        <img src="/heart.svg" alt="Heart">
+        <b>Закладки</b>
+    </li>
+
+    <li class='flex items-center gap-3'>
+        <img src="/profile.svg" alt="Profile">
+        <b>Профиль</b>
+    </li>
+</ul>
+    </header>
+
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+/* Можно оставить пустым или добавить свои стили */
+</style>
