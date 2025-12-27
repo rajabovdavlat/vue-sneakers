8bfc92de8a5b516a99503322f78ec54b19146516
rajabovdavlat <rajabovoff1@gmail.com>
2025-12-27 13:55:29 +0500

    The header layout is partially done. The store name and 3 <li> for navigation have been added: price list, bookmarks, and profile.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
 
-    <header class='flex justify-between border-b border-slate-300 px-8 py-8'>
+    <header class='flex justify-between border-b border-slate-300 px-10 py-8'>
 <div class='flex items-center gap-4'>
-    <img src="/logo.png" alt="Logo" class='w-10'>
+    <img src="/logo.png" alt="Logo" class='w-10 cursor-pointer'>
     <div>
         <h2 class='text-xl font-bold uppercase'>Vue Sneakers</h2>
         <p class='text-slate-400'>Магазин лучших крассовок</p>
@@ -11,19 +11,19 @@
 </div>
 
 <ul class='flex items-center gap-10'>
-    <li class='flex items-center gap-3'>
+    <li class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer'>
         <img src="/cart.svg" alt="Cart">
         <b>1285 руб.</b>
     </li>
 
-     <li class='flex items-center gap-3'>
+     <li class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer'>
         <img src="/heart.svg" alt="Heart">
-        <b>Закладки</b>
+        <span>Закладки</span>
     </li>
 
-    <li class='flex items-center gap-3'>
+    <li class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer'>
         <img src="/profile.svg" alt="Profile">
-        <b>Профиль</b>
+        <span>Профиль</span>
     </li>
 </ul>
     </header>
