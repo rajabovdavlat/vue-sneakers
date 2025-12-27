992579f38ab5a063720561bdf2c44bc0ce661eab
rajabovdavlat <rajabovoff1@gmail.com>
2025-12-27 18:02:37 +0500

    Moving the header logic to a separate component in the components/Header.vue folder

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,12 @@
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
 <template>
   <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
-
-    <header class='flex justify-between border-b border-slate-300 px-10 py-8'>
-<div class='flex items-center gap-4'>
-    <img src="/logo.png" alt="Logo" class='w-10 cursor-pointer'>
-    <div>
-        <h2 class='text-xl font-bold uppercase'>Vue Sneakers</h2>
-        <p class='text-slate-400'>Магазин лучших крассовок</p>
-    </div>
-</div>
-
-<ul class='flex items-center gap-10'>
-    <li class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer'>
-        <img src="/cart.svg" alt="Cart">
-        <b>1285 руб.</b>
-    </li>
-
-     <li class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer'>
-        <img src="/heart.svg" alt="Heart">
-        <span>Закладки</span>
-    </li>
-
-    <li class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer'>
-        <img src="/profile.svg" alt="Profile">
-        <span>Профиль</span>
-    </li>
-</ul>
-    </header>
-
+    <Header />
   </div>
 </template>
-
-<script></script>
 
 <style scoped>
 /* Можно оставить пустым или добавить свои стили */
