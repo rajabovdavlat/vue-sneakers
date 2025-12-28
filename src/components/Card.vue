4bdfc32ccefc9e7eb1f584f8b125faa595d3eb00
rajabovdavlat <rajabovoff1@gmail.com>
2025-12-28 16:12:27 +0500

    There were changes to write the logic further with TS and also added some states to the card, namely to the add and favorite buttons

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,36 @@
-<script setup>
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  imageUrl: string;
+  price: number;
+  isFavorite: boolean;
+  isAdded: boolean;
+  onClickAdd: Function
+  onClickFavorite: Function
+
+}>();
+
 
 </script>
 
 <template>
-     <div class='relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl'>
-        <img src="/like-2.svg" alt="Like 2" class='absolute  top-8 left-8' />
-        <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
-        <p class='mt-2'>Мужские Кроссовки Nike Blazer Mid Suede</p>
-        <div class="flex justify-between">
-          <div class='flex flex-col mt-5'>
-            <span class='text-slate-400'>Цена:</span>
-            <b>1205 руб.</b>
-          </div>
-          <img src="/plus.svg" alt="Plus">
-        </div>
+  <div
+    class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img
+      :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+      alt="Like 2"
+      class="absolute top-8 left-8"
+      @click='onClickFavorite'
+    />
+    <img :src="imageUrl" alt="Sneaker" />
+    <p class="mt-2">{{ title }}</p>
+    <div class="flex justify-between">
+      <div class="flex flex-col mt-5">
+        <span class="text-slate-400">Цена:</span>
+        <b>{{ price }}</b>
       </div>
+      <img @click='onClickAdd' :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" />
+    </div>
+  </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,15 +1,21 @@
-<script setup>
+<script setup lang="ts">
 import Card from "./Card.vue";
 
+
+const onClickAdd = () => {
+  alert('Добавить!')
+}
 </script>
 
 <template>
-     <div class='grid grid-cols-4 gap-5'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      imageUrl="/sneakers/sneakers-1.jpg"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      price="1280 руб."
+     :isFavorite='false'
+     :onClickAdd='onClickAdd'
+    />
+   
+  </div>
 </template>
