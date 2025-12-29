133c4c6c4d8a751db116209ef459cdd601c7f340
rajabovdavlat <rajabovoff1@gmail.com>
2025-12-29 12:18:11 +0500

    and also moved CartItem.vue to CartItemList.vue

---
+++ src/components/CartItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex gap-4 border border-slate-200 p-4 rounded-xl">
+    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneakers" />
+    <div class="flex flex-col justify-between">
+      <p>Мужские Кроссовки Nike Air Max 270</p>
+      <div class="flex justify-between mt-2">
+        <b>12999 руб.</b>
+        <img class='opacity-40 hover:opacity-100 cursor-pointer transition' src="/close.svg" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
