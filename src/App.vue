de71c5eac61adeb35c849adddd08fd11284501d1
rajabovdavlat <rajabovoff1@gmail.com>
2025-12-29 13:15:58 +0500

    A select button was also added for selecting categories.

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,19 @@ import Drawer from "./components/Drawer.vue";
     <Header />
 
     <div class="p-10">
-      <div class="flex justify-between">
+      <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">Все крассовок</h2>
 
+
+        <select class='py-2 px-3 rounded-md outline-none border border-gray-300 shadow-lg' name="" id="">
+          <option>По названию</option>
+          <option>По цене (дешевые)</option>
+          <option>По цене (дорогие)</option>
+        </select>
         <div class='relative'>
           <img class="absolute left-4 top-3 " src="/search.svg" alt="Search" />
           <input
-            class="border border-gray-300 rounded-lg py-2 pl-10 pr-4 outline-none focus:border-slate-400"
+            class="border shadow-lg border-gray-300 rounded-lg py-2 pl-10 pr-4 outline-none focus:border-slate-400"
             type="text"
             placeholder="Поиск..."
           />
