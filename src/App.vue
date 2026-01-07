e7130896cb9658825700497e0b1b370c8f16bd9d
rajabovdavlat <rajabovoff1@gmail.com>
2026-01-07 17:15:17 +0500

    Added search and sorting functionality using the onMounted and watch hooks.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,48 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, reactive, ref, watch } from "vue";
 import axios from "axios";
 import Header from "./components/Header.vue";
 import CardList from "./components/CardList.vue";
 import Drawer from "./components/Drawer.vue";
 
 const items = ref([]);
-onMounted(async () => {
-  //   fetch("https://de8fff53e0466791.mokky.dev/items")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //   console.log(data)
-  // })
 
+const filters = reactive({
+  sortBy: "title",
+  searchQuery: "",
+});
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value;
+};
+
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
   try {
+    const params = {
+      sortBy: filters.sortBy,
+    };
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
     const { data } = await axios.get(
-      "https://de8fff53e0466791.mokky.dev/items"
+      `https://de8fff53e0466791.mokky.dev/items`,
+      {
+        params,
+      }
     );
     items.value = data;
   } catch (err) {
     console.log(err);
   }
-});
+};
+
+onMounted(fetchItems);
+
+watch(filters, fetchItems);
 </script>
 
 <template>
@@ -35,17 +56,19 @@ onMounted(async () => {
 
         <div class="flex gap-4">
           <select
+            @change="onChangeSelect"
             class="py-2 px-3 rounded-lg outline-none border border-gray-300 shadow-lg"
             name=""
             id=""
           >
-            <option>По названию</option>
-            <option>По цене (дешевые)</option>
-            <option>По цене (дорогие)</option>
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешевые)</option>
+            <option value="-price">По цене (дорогие)</option>
           </select>
           <div class="relative">
             <img class="absolute left-4 top-3" src="/search.svg" alt="Search" />
             <input
+            @input='onChangeSearchInput'
               class="border shadow-lg border-gray-300 rounded-lg py-2 pl-10 pr-4 outline-none focus:border-slate-400"
               type="text"
               placeholder="Поиск..."
